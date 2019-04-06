c9c6ed37a1a0acfd8e9e4f2b37c1a8434b1d9386
Per Kraulis <per.kraulis@gmail.com>
2019-04-06 19:43:22 +0200

    Bumped minor version.

--- pleko/__init__.py
+++ pleko/__init__.py
@@ -4,7 +4,7 @@ Web tool to explore, publish and plot data sets using Sqlite3 and Vega-Lite.
 
 import os.path
 
-__version__ = '0.7.9'
+__version__ = '0.8.0'
 
 ROOT_DIR = os.path.dirname(__file__)
 
