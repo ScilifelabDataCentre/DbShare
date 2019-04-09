21fd5425dda9318fbc1f7e5baa17e358a3bf2633
Per Kraulis <per.kraulis@scilifelab.se>
2019-04-09 11:07:31 +0200

    Got rid of explicit SQL execution. Closes #181.

--- pleko/query.py
+++ pleko/query.py
@@ -22,16 +22,10 @@ def home(dbname):
         return flask.redirect(flask.url_for('home'))
     has_write_access = pleko.db.has_write_access(db)
     query = get_query_from_request(check=False)
-    if query.get('select'):
-        sql = None
-    else:
-        sql = flask.request.args.get('sql')
-        if sql: sql = sql.strip()
     cnx = pleko.db.get_cnx(dbname)
     return flask.render_template('query/home.html',
                                  db=db,
                                  query=query,
-                                 sql=sql,
                                  has_write_access=has_write_access)
 
 @blueprint.route('/<name:dbname>/rows', methods=['POST'])
@@ -73,42 +67,6 @@ def rows(dbname):
                                  sql=sql,
                                  rows=rows)
 
-@blueprint.route('/<name:dbname>/sql', methods=['POST'])
-def sql(dbname):
-    """Execute a complete SQL statement for the database.
-    The set of allowed SQL commands excludes the clearly damaging ones.
-    """
-    utils.check_csrf_token()
-    try:
-        db = pleko.db.get_check_write(dbname)
-    except ValueError as error:
-        flask.flash(str(error), 'error')
-        return flask.redirect(flask.url_for('home'))
-    sql = flask.request.form.get('sql')
-    try:
-        if not sql:
-            raise ValueError('no SQL statement provided')
-        command = sql.split()[0].upper()
-        cursor = pleko.db.get_cnx(dbname).cursor()
-        cursor.execute(sql)
-        rows = list(cursor)
-        # Will raise IndexError if no rows returned.
-        columns = ["column%i" % (i+1) for i in range(len(rows[0]))]
-    except IndexError:
-        flask.flash('no result returned', 'message')
-        return flask.redirect(flask.url_for('.home', dbname=dbname, sql=sql))
-    except (ValueError, sqlite3.Error, sqlite3.Warning) as error:
-        flask.flash(str(error), 'error')
-        return flask.redirect(flask.url_for('.home',
-                                            dbname=dbname,
-                                            sql=sql or ''))
-    return flask.render_template('query/rows.html',
-                                 db=db,
-                                 query={},
-                                 columns=columns,
-                                 sql=sql,
-                                 rows=rows)
-        
 @blueprint.route('/<name:dbname>/table', methods=['GET', 'POST'])
 @pleko.user.login_required
 def table(dbname):
