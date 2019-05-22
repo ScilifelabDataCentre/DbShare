f45ff850dcb4b14e67daa3ea1fdc30d188df266b
Per Kraulis <per.kraulis@scilifelab.se>
2019-05-22 14:16:54 +0200

    Added test of API db edit. Closes #303. Closes #304.

--- dbshare/test/db.py
+++ dbshare/test/db.py
@@ -31,19 +31,15 @@ class Db(Base):
         self.assertEqual(response.status_code, http.client.NO_CONTENT)
 
     def test_upload(self):
-        "Create a database by file upload, check its JSON, and delete it."
+        "Create a database by file upload, check its JSON."
         response = self.upload_file()
 
         # Valid API db JSON.
         jsonschema.validate(instance=response.json(),
                             schema=dbshare.schema.db.schema)
 
-        # Delete the database.
-        response = self.session.delete(self.db_url)
-        self.assertEqual(response.status_code, http.client.NO_CONTENT)
-
     def test_edit(self):
-        "Create an empty database, edit it, check its JSON, and delete it."
+        "Create an empty database, edit it, check its JSON."
 
         # Create an empty database.
         response = self.session.put(self.db_url)
@@ -74,9 +70,17 @@ class Db(Base):
         # Same title as before.
         self.assertEqual(data.get('title'), title)
 
-        # Delete the database.
-        response = self.session.delete(self.db_url)
-        self.assertEqual(response.status_code, http.client.NO_CONTENT)
+        # Rename the database; record its new url.
+        name = 'test2'
+        response = self.session.post(self.db_url, json={'name': name})
+        self.assertEqual(response.status_code, http.client.OK)
+        data = response.json()
+        jsonschema.validate(instance=response.json(),
+                            schema=dbshare.schema.db.schema)
+        self.assertTrue(data['$id'].endswith(name))
+        # So that the database is deleted at cleanup.
+        self.db_url = data['$id']
+        self.addCleanup(self.delete_db)
 
 
 if __name__ == '__main__':
