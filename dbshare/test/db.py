eff02e12b1d716dabe895c0b35146309314dd80b
Per Kraulis <per.kraulis@gmail.com>
2019-05-22 10:30:39 +0200

    Working on db edit.

--- dbshare/test/db.py
+++ dbshare/test/db.py
@@ -15,9 +15,6 @@ class Db(Base):
         super().setUp()
         self.db_url = f"{CONFIG['root_url']}/db/{CONFIG['dbname']}"
 
-    def delete_db(self):
-        self.session.delete(self.db_url)
-
     def test_create(self):
         "Create an empty database, check its JSON, and delete it."
 
@@ -40,6 +37,42 @@ class Db(Base):
         # Valid API db JSON.
         jsonschema.validate(instance=response.json(),
                             schema=dbshare.schema.db.schema)
+
+        # Delete the database.
+        response = self.session.delete(self.db_url)
+        self.assertEqual(response.status_code, http.client.NO_CONTENT)
+
+    def test_edit(self):
+        "Create an empty database, edit it, check its JSON, and delete it."
+
+        # Create an empty database.
+        response = self.session.put(self.db_url)
+        self.assertEqual(response.status_code, http.client.OK)
+
+        # Valid API db JSON.
+        jsonschema.validate(instance=response.json(),
+                            schema=dbshare.schema.db.schema)
+
+        # Edit the title.
+        title = 'New title'
+        response = self.session.post(self.db_url, json={'title': title})
+        self.assertEqual(response.status_code, http.client.OK)
+        data = response.json()
+        jsonschema.validate(instance=response.json(),
+                            schema=dbshare.schema.db.schema)
+        self.assertEqual(data.get('title'), title)
+
+        # Edit the description.
+        description = 'A description'
+        response = self.session.post(self.db_url,
+                                     json={'description': description})
+        self.assertEqual(response.status_code, http.client.OK)
+        data = response.json()
+        jsonschema.validate(instance=response.json(),
+                            schema=dbshare.schema.db.schema)
+        self.assertEqual(data.get('description'), description)
+        # Same title as before.
+        self.assertEqual(data.get('title'), title)
 
         # Delete the database.
         response = self.session.delete(self.db_url)
