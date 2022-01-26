4cbff09ac695aa48284af5cc5d78aeb4e9f90f0b
Per Kraulis <per.kraulis@gmail.com>
2022-01-26 17:52:41 +0100

    Minor mods.

--- tests/test_browser_anonymous.py
+++ tests/test_browser_anonymous.py
@@ -44,9 +44,8 @@ def test_about(settings, page): # 'page' fixture from 'pytest-playwright'
     page.set_default_timeout(4000)
     page.goto(settings['BASE_URL'])
     page.click("text=About")
-    with page.expect_navigation():
-        page.click("text=Overview")
-        assert page.url == f"{settings['BASE_URL']}/about/doc/overview"
+    page.click("text=Overview")
+    assert page.url == f"{settings['BASE_URL']}/about/doc/overview"
 
     page.goto(settings['BASE_URL'])
     page.click("text=About")
@@ -54,20 +53,20 @@ def test_about(settings, page): # 'page' fixture from 'pytest-playwright'
     assert page.url == f"{settings['BASE_URL']}/about/doc/tutorial"
     page.click("text=Explore a database")
     assert page.url == f"{settings['BASE_URL']}/about/doc/tutorial-explore"
-    page.go_back()
 
+    page.go_back()
     page.click("text=Create and modify a database")
     assert page.url == f"{settings['BASE_URL']}/about/doc/tutorial-create"
-    page.go_back()
 
+    page.go_back()
     page.click("text=Query and view")
     assert page.url == f"{settings['BASE_URL']}/about/doc/tutorial-query"
-    page.go_back()
 
+    page.go_back()
     page.click("text=Using the API")
     assert page.url == f"{settings['BASE_URL']}/about/doc/tutorial-api"
-    page.go_back()
 
+    page.go_back()
     page.click("text=About")
     page.click("text=URL endpoints")
     assert page.url == f"{settings['BASE_URL']}/about/endpoints"
@@ -86,60 +85,60 @@ def test_about_json_schema(settings, page):
 
     page.click("text=root")
     assert page.url == f"{settings['BASE_URL']}/api/schema/root"
-    page.go_back()
 
+    page.go_back()
     page.click("id=dbs")
     assert page.url == f"{settings['BASE_URL']}/api/schema/dbs"
-    page.go_back()
 
+    page.go_back()
     page.click("id=db")
     assert page.url == f"{settings['BASE_URL']}/api/schema/db"
-    page.go_back()
 
+    page.go_back()
     page.click("text=db/edit")
     assert page.url == f"{settings['BASE_URL']}/api/schema/db/edit"
-    page.go_back()
 
+    page.go_back()
     page.click("text=table")
     assert page.url == f"{settings['BASE_URL']}/api/schema/table"
-    page.go_back()
 
+    page.go_back()
     page.click("text=table/statistics")
     assert page.url == f"{settings['BASE_URL']}/api/schema/table/statistics"
-    page.go_back()
 
+    page.go_back()
     page.click("text=table/create")
     assert page.url == f"{settings['BASE_URL']}/api/schema/table/create"
-    page.go_back()
 
+    page.go_back()
     page.click("text=table/input")
     assert page.url == f"{settings['BASE_URL']}/api/schema/table/input"
-    page.go_back()
 
+    page.go_back()
     page.click("text=view View API JSON schema. >> a")
     assert page.url == f"{settings['BASE_URL']}/api/schema/view"
-    page.go_back()
 
+    page.go_back()
     page.click("text=view/create")
     assert page.url == f"{settings['BASE_URL']}/api/schema/view/create"
-    page.go_back()
 
+    page.go_back()
     page.click("text=rows")
     assert page.url == f"{settings['BASE_URL']}/api/schema/rows"
-    page.go_back()
 
+    page.go_back()
     page.click("text=query/input")
     assert page.url == f"{settings['BASE_URL']}/api/schema/query/input"
-    page.go_back()
 
+    page.go_back()
     page.click("text=query/output")
     assert page.url == f"{settings['BASE_URL']}/api/schema/query/output"
-    page.go_back()
 
+    page.go_back()
     page.click("text=user")
     assert page.url == f"{settings['BASE_URL']}/api/schema/user"
-    page.go_back()
 
+    page.go_back()
     page.click("text=users")
     assert page.url == f"{settings['BASE_URL']}/api/schema/users"
     page.go_back()
